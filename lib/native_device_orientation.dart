29e13a8e1df1bfe5d21c06afac80dbd660f719d0
noriHanda <noriakihanda@gmail.com>
2024-01-21 22:32:03 +0900

    Revert "Add a getter that returns `DeviceOrientation` to `NativeDeviceOrientation`"
    This reverts commit e6a986964727eb1e06dc8d0128673ddb62a31020.

--- lib/native_device_orientation.dart
+++ lib/native_device_orientation.dart
@@ -3,31 +3,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
-enum NativeDeviceOrientation {
-  portraitUp,
-  portraitDown,
-  landscapeLeft,
-  landscapeRight,
-  unknown;
-
-  const NativeDeviceOrientation();
-
-  /// Returns corresponding [DeviceOrientation] for this [NativeDeviceOrientation]
-  DeviceOrientation? get deviceOrientation {
-    switch (this) {
-      case NativeDeviceOrientation.portraitUp:
-        return DeviceOrientation.portraitUp;
-      case NativeDeviceOrientation.portraitDown:
-        return DeviceOrientation.portraitDown;
-      case NativeDeviceOrientation.landscapeLeft:
-        return DeviceOrientation.landscapeLeft;
-      case NativeDeviceOrientation.landscapeRight:
-        return DeviceOrientation.landscapeRight;
-      case NativeDeviceOrientation.unknown:
-        return null;
-    }
-  }
-}
+enum NativeDeviceOrientation { portraitUp, portraitDown, landscapeLeft, landscapeRight, unknown }
 
 class _OrientationStream {
   final Stream<NativeDeviceOrientation> stream;
